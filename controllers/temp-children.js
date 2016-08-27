dfeda1b68bd9bcb48f5f4965a99c2f6d414abb30
John Marion <John Marion>
2016-08-27 18:13:07 -0400

    temp-children.js

---
+++ controllers/temp-children.js
@@ -0,0 +1,69 @@
+
+var logger = require ( '../../global/logger' );
+var apigen = require ( '../../global/lib/apigen' );
+var authFunction = require ( '../../auth/lib/auth-function')
+var Child = require ( '../models/child' );
+
+module.exports = apigen ({
+    authFunction: authFunction,
+    collection: Child,
+    endpoint: '/children',
+    nameField: 'name',
+    objectName: 'Student',
+    options: {
+        GETALL: {
+            requiresAuthentication: true,
+            queryBuilder: function ( res, user )
+            {
+                return {
+                    $query: {
+                        user: user.username
+                    },
+                    $orderby: {
+                        'age': -1
+                    }
+                };
+            }
+        },
+        GET: {
+            field: '_id',
+            requiresAuthentication: true
+        },
+        POST: {
+            objectBuilder: function ( req, user )
+            {
+                var newObject = {
+                    name: req.body.name,
+                    user: user.username
+                };
+                if ( req.body.age != undefined ) newObject.age = req.body.age;
+                if ( req.body.grade != undefined )
+                    newObject.grade = req.body.grade;
+                return newObject;
+            },
+            requiresAuthentication: true
+        },
+        PUT: {
+            field: '_id',
+            queryBuilder: function ( req, user )
+            {
+                return {
+                    '_id': req.params.value,
+                    'user': user.username
+                };
+            },
+            requiresAuthentication: true
+        },
+        DELETE: {
+            field: '_id',
+            queryBuilder: function ( req, user )
+            {
+                return {
+                    '_id': req.params.value,
+                    'user': user.username
+                }
+            },
+            requiresAuthentication: true
+        }
+    }
+});
